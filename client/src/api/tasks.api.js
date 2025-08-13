440fd8d13e2c34014d822e54d0dbca1f26c1ffad
arleyortega25 <oarleyfernando21@gmail.com>
2025-08-12 21:12:07 -0500

    fixing url

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,19 +1,19 @@
 import axios from "./axios.js";
 
 export const ApiTareaGet = async () => {
-  return await axios.get("deploy-backend-mern-mongodb-production.up.railway.app/api/tareas");
+  return await axios.get("https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/api/tareas");
 };
 
 export const  ApiTareaPost = async (task) => {
-  return await axios.post("deploy-backend-mern-mongodb-production.up.railway.app/api/tareas", task);
+  return await axios.post("https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/api/tareas", task);
 };
 
 export const ApiTareaGetById = (id) => {
-  return axios.get(`deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`);
+  return axios.get(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/api/tareas/${id}`);
 };
 export const ApiTareaDelete = (id) => {
-  return axios.delete(`deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`);
+  return axios.delete(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/api/tareas/${id}`);
 };
 export const ApiTareaPut = (id, task) => {
-  return axios.put(`deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`, task);
+  return axios.put(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/api/tareas/${id}`, task);
 };
