8c30a32c95dbca2aed4ea9cd25fd471102dbaefa
arleyortega25 <oarleyfernando21@gmail.com>
2025-08-12 22:23:53 -0500

    fix cors 4

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,27 +1,20 @@
 import axios from "./axios.js";
-const getAuthHeaders = () => {
-  const token = localStorage.getItem("token"); // o sessionStorage
-  return {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-};
+
 
 export const ApiTareaGet = async () => {
-  return await axios.get("https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas",getAuthHeaders());
+  return await axios.get("https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas");
 };
 
 export const  ApiTareaPost = async (task) => {
-  return await axios.post("https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas", task,getAuthHeaders());
+  return await axios.post("https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas", task);
 };
 
 export const ApiTareaGetById = (id) => {
-  return axios.get(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`,getAuthHeaders());
+  return axios.get(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`);
 };
 export const ApiTareaDelete = (id) => {
-  return axios.delete(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`,getAuthHeaders());
+  return axios.delete(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`);
 };
 export const ApiTareaPut = (id, task) => {
-  return axios.put(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`, task,getAuthHeaders());
+  return axios.put(`https://deploy-backend-mern-mongodb-production.up.railway.app/api/tareas/${id}`, task);
 };
